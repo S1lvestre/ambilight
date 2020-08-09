9b2288328ee33abe1c0cf574d7c7c6a071eb7b57
Sil <s.a.piedade@gmail.com>
2020-08-09 22:50:00 +0100

    bug fixes

--- main.py
+++ main.py
@@ -10,18 +10,25 @@ import numpy as np
 import serial
 import time
 
+
+
 #=========================
 #
 # VARIABLES
 #
 #=========================
 
+numLEDS = 84
+zonesCount = [26, 16]    # top/bottom, left/right
+zonesDism = [72, 36, 24] # length, width, offset
+
+# Part of the screen to capture
 monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
+
+
 
 startMarker = ord('<')
 endMarker = ord('>')
-
-
 
 
 
@@ -30,6 +37,29 @@ endMarker = ord('>')
 # FUNCTION DEFINITIONS
 #
 #=========================
+
+def calcAvrg(array) :
+    """ receives (i,j,3) array, 
+        turns into i*j by 3,
+        calcs avrg for each channel """
+    
+    elmCount = np.shape(array)[0]*np.shape(array)[1]
+
+    data = np.reshape( array, (elmCount, 3) )
+    
+    color = np.round(np.sum( data, axis = 0 ) / elmCount).astype(np.uint8)
+
+    return color
+
+
+def rgbToStr(rgb) :
+    """  """
+
+    string = '0x%02x%02x%02x;' % (rgb[0], rgb[1], rgb[2])
+
+    return string
+
+
 
 def waitForMessage(message):
     """ wait untill the arduino sends "message"
@@ -73,46 +103,73 @@ print("\nSerial port /dev/ttyACM0 opened;\nBaudrate 9600\n")
 waitForMessage("Arduino is Ready")
 arduinoIsReady = True
 
-##########################################
-#colors = "<0x000000;0x000000;0x000000;>"#
-##########################################
-
 while True:
     if arduinoIsReady :
         # get frame of colors
         with mss.mss() as sct:
+            goingOut = "<"
+
+            # Get raw pixels from the screen, save it to a Numpy array
             img = np.array(sct.grab(monitor))[:,:,:3]
-#         ->
-#         ->
-#         ->
-#         ->
-#         ->
-#         ->
-#         ->
+            
+            # top row
+            for top_i in range(zonesCount[0]) :
+                zone = img[0:15, (72*top_i)+24 : (72*(top_i+1))+24]
+                avrgColor = calcAvrg(zone)
+                goingOut += rgbToStr(avrgColor)
+            
+            #top right
+            zone = img[0:15, -15:1920]
+            avrgColor = calcAvrg(zone)
+            goingOut += rgbToStr(avrgColor)
+            
+            # right row
+            for right_i in range(zonesCount[1]-2):
+                zone = img[(72*right_i)+15 : (72*(right_i+1))+15 , -15:1920 ]
+                avrgColor = calcAvrg(zone)
+                goingOut += rgbToStr(avrgColor)
+            
+            #bot right
+            zone = img[-15:1080, -15:1920]
+            avrgColor = calcAvrg(zone)
+            goingOut += rgbToStr(avrgColor)
 
-            #color = qq coisa
+            # bottom row
+            for bot_i in range(zonesCount[0]) :
+                zone = img[-15:1080, (72*bot_i)+24 : (72*(bot_i+1))+24  ]
+                avrgColor = calcAvrg(zone) 
+                goingOut += rgbToStr(avrgColor)
+            
+            #bot left
+            zone = img[-15:1080, 0:15]
+            avrgColor = calcAvrg(zone)
+            goingOut += rgbToStr(avrgColor)
 
+            # left row
+            for left_i in range(zonesCount[1]-2):
+                zone = img[(72*left_i)+15 : (72*(left_i+1))+15 , 0:36 ]
+                avrgColor = calcAvrg(zone)
+                goingOut += rgbToStr(avrgColor)
+            
+            #top left
+            zone = img[0:15, 0:15]
+            avrgColor = calcAvrg(zone)
+            goingOut += rgbToStr(avrgColor)
+            
+            goingOut += '>'
+        
         # send next frame of colors
-#     ->ser.write(colors.encode())
+        ser.write(goingOut.encode())
         arduinoIsReady = False
         
-        #print( "Test " + str(n) + '\n'        )
-        #print( "╔═══════ PC ══════════════"   )
-        #print( "║     Sent: " + colors        )
-        #print( "╚═════════════════════════\n" )
+        print( "╔═══════ PC ══════════════"   )
+        print( "║     Sent: " + goingOut      )
+        print( "╚═════════════════════════\n" )
 
-    else
+    else :
         # wait for signal from arduino
         waitForMessage("Send Next")
         arduinoIsReady = True
-
-
-
-
-
-
-
-
 
 ser.close
 print("Serial port /dev/ttyACM0 closed;\n")
