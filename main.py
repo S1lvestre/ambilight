ae8da419f431fd5d4453c61af6b61fdf1fc5358c
Sil <s.a.piedade@gmail.com>
2020-08-10 23:00:21 +0100

    changed tv layout

--- main.py
+++ main.py
@@ -20,7 +20,10 @@ import time
 
 numLEDS = 84
 zonesCount = [26, 16]    # top/bottom, left/right
-zonesDism = [72, 36, 24] # length, width, offset
+
+zonesL = 72
+zonesW = 10
+zonesOffst = 24
 
 # Part of the screen to capture
 monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
@@ -104,58 +107,46 @@ waitForMessage("Arduino is Ready")
 arduinoIsReady = True
 
 while True:
-    if arduinoIsReady :
+    # This isn't running fast enough for this if else to make sense...
+    # yet
+    if True : #arduinoIsReady :
         # get frame of colors
         with mss.mss() as sct:
             goingOut = "<"
 
             # Get raw pixels from the screen, save it to a Numpy array
             img = np.array(sct.grab(monitor))[:,:,:3]
-            
-            # top row
-            for top_i in range(zonesCount[0]) :
-                zone = img[0:15, (72*top_i)+24 : (72*(top_i+1))+24]
-                avrgColor = calcAvrg(zone)
-                goingOut += rgbToStr(avrgColor)
-            
-            #top right
-            zone = img[0:15, -15:1920]
-            avrgColor = calcAvrg(zone)
-            goingOut += rgbToStr(avrgColor)
-            
-            # right row
-            for right_i in range(zonesCount[1]-2):
-                zone = img[(72*right_i)+15 : (72*(right_i+1))+15 , -15:1920 ]
-                avrgColor = calcAvrg(zone)
-                goingOut += rgbToStr(avrgColor)
-            
-            #bot right
-            zone = img[-15:1080, -15:1920]
-            avrgColor = calcAvrg(zone)
-            goingOut += rgbToStr(avrgColor)
-
-            # bottom row
-            for bot_i in range(zonesCount[0]) :
-                zone = img[-15:1080, (72*bot_i)+24 : (72*(bot_i+1))+24  ]
-                avrgColor = calcAvrg(zone) 
-                goingOut += rgbToStr(avrgColor)
-            
+           
             #bot left
-            zone = img[-15:1080, 0:15]
-            avrgColor = calcAvrg(zone)
-            goingOut += rgbToStr(avrgColor)
-
-            # left row
-            for left_i in range(zonesCount[1]-2):
-                zone = img[(72*left_i)+15 : (72*(left_i+1))+15 , 0:36 ]
+            for i in range(9):
+                zone = img[-50:1080 , 584-(73*i):658-(73*i)]
                 avrgColor = calcAvrg(zone)
                 goingOut += rgbToStr(avrgColor)
             
-            #top left
-            zone = img[0:15, 0:15]
-            avrgColor = calcAvrg(zone)
-            goingOut += rgbToStr(avrgColor)
-            
+            #left
+            for i in range(16):
+                zone = img[ 1080-(67*i):1013-(67*i) , 0:50]
+                avrgColor = calcAvrg(zone)
+                goingOut += rgbToStr(avrgColor)
+
+            # top row
+            for top_i in range(26) :
+                zone = img[0:50, (73*top_i): (73*(top_i+1))]
+                avrgColor = calcAvrg(zone)
+                goingOut += rgbToStr(avrgColor)
+
+            #right
+            for i in range(16):
+                zone = img[ 0+(67*i):67+(67*i) , -50:1920]
+                avrgColor = calcAvrg(zone)
+                goingOut += rgbToStr(avrgColor)
+
+            #bot right
+            for i in range(9):
+                zone = img[-50:1080 , 1847-(73*i):1921-(73*i)]
+                avrgColor = calcAvrg(zone)
+                goingOut += rgbToStr(avrgColor)
+
             goingOut += '>'
         
         # send next frame of colors
@@ -166,10 +157,11 @@ while True:
         print( "║     Sent: " + goingOut      )
         print( "╚═════════════════════════\n" )
 
-    else :
+    # This isn't running fast enough for this if else to make sense...
+    #else :
         # wait for signal from arduino
-        waitForMessage("Send Next")
-        arduinoIsReady = True
+    #    waitForMessage("Send Next")
+    #    arduinoIsReady = True
 
 ser.close
 print("Serial port /dev/ttyACM0 closed;\n")
